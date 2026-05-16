f0479e7bbb9a273ce0c1aa45c49715c86c70db9e
Vaishnavi <sinhavaishnavi020@gmail.com>
2026-05-16 23:40:59 +0530

    feat:Chunk texts

--- app/services/rag_service.py
+++ app/services/rag_service.py
@@ -2,11 +2,13 @@ import os
 import uuid
 from fastapi import UploadFile
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 UPLOAD_DIR="temp_uploads"
 
 os.makedirs(UPLOAD_DIR,exist_ok=True)
 
+# UPLOADING PDF
 async def process_pdf(file:UploadFile):
     if not file.filename.endswith(".pdf"):
         return{
@@ -21,12 +23,20 @@ async def process_pdf(file:UploadFile):
             buffer.write(content)
         loader=PyPDFLoader(file_path)
         documents=loader.load()
+
+        chunks=chunk_texts(documents)
+
         preview_text=(documents[0].page_content[:500])
 
         return {
             "message":"PDF Uploaded successfully.",
             "total_pages":len(documents),
-            "preview":preview_text
+            "total_chuks":len(chunks),
+            "preview":preview_text,
+            "chunk_preview":[
+                chunk.page_content[:200]
+                for chunk in chunks[:3]
+            ]
         }
     except Exception as e:
         return {
@@ -36,3 +46,20 @@ async def process_pdf(file:UploadFile):
     finally:
         if os.path.exists(file_path):
             os.remove(file_path)
+
+# CHUNKING DOCUMENTS
+def chunk_texts(doc):
+    splitter=RecursiveCharacterTextSplitter(
+            chunk_size=800,
+            chunk_overlap=160,
+            separators=[
+                "\n\n",
+                "\n",
+                ".",
+                " ",
+                ""
+            ]
+        )
+    chunks=splitter.split_documents(doc)
+        
+    return chunks
